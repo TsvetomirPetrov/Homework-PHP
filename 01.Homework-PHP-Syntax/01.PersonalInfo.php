ed94b8bc28dad386e273abb5cd4f841f76b95382
TsvetomirPetrov <petrov.tsvetomir1@gmail.com>
2014-08-14 11:44:26 +0300

    first

---
+++ 01.Homework-PHP-Syntax/01.PersonalInfo.php
@@ -0,0 +1,7 @@
+<?php
+    $firstName = 'Gosho';
+    $lastName = 'Goshev';
+    $age = 20;
+    $fullName = $firstName .' '. $lastName;
+    echo "My name is $fullName and I am $age years old.";
+?>
---
+++ 01.Homework-PHP-Syntax/02.SumTwoNumbers.php
@@ -0,0 +1,5 @@
+<?php
+    $firsNumber = 1.567808;
+    $secondNumber = 0.356;
+    echo '$firsNumber + $secondNumber = '.$firsNumber.' + '.$secondNumber.' = '.round($firsNumber + $secondNumber,2);
+?>
